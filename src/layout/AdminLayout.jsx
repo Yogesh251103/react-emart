14544bd62e498337f44f140e78051623824df6b3
Ex-Rockstar <ruthirockstar2218@gmail.com>
2025-06-20 14:37:22 +0530

    table component

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,10 +1,12 @@
 import AdminSidebar from "../components/admin/SideBar";
 import { Outlet } from "react-router-dom";
+ 
 
 export default function AdminLayout() {
   return (
     <div className="w-screen flex overflow-x-hidden">
       <AdminSidebar />
+      
       <main className="w-[70%]">
         <Outlet />
       </main>
--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -1,8 +1,18 @@
-import Sidebar from "../../components/admin/SideBar"
-
+// import Sidebar from "../../components/admin/SideBar"
+import Tablee from "../../components/admin/Table"
 const Home = () => {
   return (
-    <div>
+    <div style={{
+        display: "flex",
+        justifyContent: "center", // horizontally center
+        alignItems: "center",     // vertically center
+        height: "100vh",          // fill the full screen
+        backgroundColor: "#f5f5f5", // optional soft background
+      }}>
+      <div style={{ width: "80%", maxWidth: "1000px" }}>
+      <Tablee/>
+      </div>
+      
 
     </div>
   )
