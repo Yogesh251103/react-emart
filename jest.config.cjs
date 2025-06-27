f37e0cae90eb514fc8646ecb7be133fd746b62b0
Ex-Rockstar <ruthirockstar2218@gmail.com>
2025-06-27 10:53:53 +0530

    testing set up

---
+++ jest.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
