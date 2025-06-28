9fe260eb1030cbdcb9b05d60712cba046a7967ee
guru sanjay <gurucoolmsd@gmail.com>
2025-06-28 09:46:11 +0530

    insights api button in dashboard for testing

--- src/pages/vendor/Home.jsx
+++ src/pages/vendor/Home.jsx
@@ -1,10 +1,12 @@
 import SaleInsights from "@/components/vendor/SaleInsights";
+import VendorBot from "@/components/vendor/VendorBot";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
       <SaleInsights />
+      <VendorBot/>
     </div>
   );
 };
