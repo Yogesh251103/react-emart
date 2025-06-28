501130df7681a290e7662bccf4d7743a99641d22
guru sanjay <gurucoolmsd@gmail.com>
2025-06-28 09:43:09 +0530

    integrated insights api

---
+++ src/components/vendor/VendorBot.jsx
@@ -0,0 +1,54 @@
+import { useSnackbar } from '@/contexts/SnackbarContexts';
+import useAxios from '@/hooks/useAxios/useAxios'
+import React, { useEffect, useState } from 'react'
+
+function VendorBot() {
+  const { fetchData} = useAxios();
+  const showSnackbar = useSnackbar();
+  const [insightData,setInsightData] = useState([])
+  const [insights,setInsights] = useState([])
+  const token = localStorage.getItem("vendorToken")
+  const fetchInsights = async ()=> {
+    try {
+      const insightsRes = await fetchData({
+        method: "GET",
+        url: "/vendor/outlet/sale/insights",
+        headers: {
+          Authorization : `Bearer ${token}`
+        }
+      })
+      if (insightsRes){
+        console.log(insightsRes)
+        setInsightData(insightsRes)
+        try {
+          console.log("Fetched Data",insightData)
+          const groqRes = await fetchData({
+            method: "POST",
+            url: `${import.meta.env.VITE_APP_GROQ_API}/groq`,
+            data: insightData
+          })
+          if(groqRes){
+            console.log("Groq response",groqRes);
+            setInsights(groqRes)
+          }
+        }
+        catch(err){
+          showSnackbar(err?.message || "Error fetching groq response",'error')
+        }
+      }
+    } 
+    catch (err) {
+      showSnackbar(err?.message || "Error fetching insights", 'error')
+
+    }
+  }
+  return (
+    <div> 
+      <button onClick={fetchInsights}>
+        fetch insights
+      </button>
+    </div>
+  )
+}
+
+export default VendorBot
